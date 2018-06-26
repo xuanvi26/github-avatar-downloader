6e1e22a0b632ba2e8d70095559f74c18a3f06488
xuanvi26 <xuanvi.trinhnguyen@gmail.com>
2018-06-26 12:11:34 -0400

    Implement download image by URL

--- download_avatars.js
+++ download_avatars.js
@@ -2,36 +2,34 @@ var request = require('request');
 var secrets = require('./secrets')
 var fs = require('fs');
 
-// console.log('Welcome to the GitHub Avatar Downloader!');
+console.log('Welcome to the GitHub Avatar Downloader!');
 
-// function getRepoContributors(repoOwner, repoName, cb) {
-//     let options = {
-//         url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-//         headers: {
-//             'User-Agent': 'request',
-//             authorization: secrets.GITHUB_TOKEN
-//         }
-//     }
+function getRepoContributors(repoOwner, repoName, cb) {
+    let options = {
+        url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+        headers: {
+            'User-Agent': 'request',
+            authorization: secrets.GITHUB_TOKEN
+        }
+    }
     
-//     request(options, function(err, res, body) {
-//         cb(err, body);
-//     })
-// }
-
-// getRepoContributors("jquery", "jquery", function(err, result) {
-//     console.log("Errors:", err);
-//     let jsonResObj = JSON.parse(result);
-//     for (let contributor of jsonResObj) {
-//         console.log (contributor.avatar_url);
-//     }
-// });
+    request(options, function(err, res, body) {
+        cb(err, body);
+    })
+}
 
 function downloadImageByURL(url, filePath) {
     request.get(url)
     .on('error', function (err) {
         throw err;
     })
-    .pipe(fs.createWriteStream('./' + filePath))
+    .pipe(fs.createWriteStream('./avatars/' + filePath + '.jpg'))
 }
 
-downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
+getRepoContributors("jquery", "jquery", function(err, result) {
+    console.log("Errors:", err);
+    let jsonResObj = JSON.parse(result);
+    for (let contributor of jsonResObj) {
+        downloadImageByURL(contributor.avatar_url, contributor.login);
+    }
+});
