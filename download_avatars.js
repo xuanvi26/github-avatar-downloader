2ef289d17323df4892ea092f5112a6f7d8f5c91d
xuanvi26 <xuanvi.trinhnguyen@gmail.com>
2019-04-26 17:41:02 -0400

    End write stream when on error of file path

--- download_avatars.js
+++ download_avatars.js
@@ -1,16 +1,16 @@
-var request = require('request')
-var fs = require('fs')
-var args = process.argv.slice(2)
-require('dotenv').config()
+var request = require('request');
+var fs = require('fs');
+var args = process.argv.slice(2);
+require('dotenv').config();
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function printArgError() {
-    if (args[0] === undefined || args[1] === undefined) throw new Error('You did not input the repo owner and name. Please input both in the correct order (owner, name)')
-    if (args[2] !== undefined) console.log('You input too many arguments. Please input two (owner, name.')
+    if (args[0] === undefined || args[1] === undefined) throw new Error('You did not input the repo owner and name. Please input both in the correct order (owner, name)');
+    if (args[2] !== undefined) console.log('You input too many arguments. Please input two (owner, name.');
 }
 
-printArgError()
+printArgError();
 
 function getRepoContributors(repoOwner, repoName, cb) {
     let options = {
@@ -19,20 +19,29 @@ function getRepoContributors(repoOwner, repoName, cb) {
             'User-Agent': 'request',
             authorization: process.env.GITHUB_TOKEN
         }
-    }
+    };
     
     request(options, function(err, res, body) {
         if (res.statusCode !== '200') throw new Error('Request failed');
-        cb(err, body)
-    })
+        cb(err, body);
+    });
 }
 
 function downloadImageByURL(url, filePath) {
+
+    let writeStream =  fs.createWriteStream('./avatars/' + filePath + '.jpg');
+    
+    writeStream.on('error', function(err) {
+        console.log('Something wrong with the file path')
+        writeStream.end();
+    });
+
     request.get(url)
     .on('error', function (err) {
         throw err;
     })
-    .pipe(fs.createWriteStream('./avatars/' + filePath + '.jpg'))
+    .pipe(writeStream);
+
 }
 
 getRepoContributors(args[0], args[1], function(err, result) {
