7e0e5a4b805d1b6e8d19f84f2d75c07834b52f38
xuanvi26 <xuanvi.trinhnguyen@gmail.com>
2019-04-29 18:16:07 -0400

    Complete recommended repo exercise

--- recommend.js
+++ recommend.js
@@ -2,7 +2,7 @@ const request = require('request');
 const args = process.argv.slice(2);
 require('dotenv').config();
 
-function fetchData(repoOwner, repoName, cb) {
+function fetchUserData(repoOwner, repoName, cb, cb2) {
     let options = {
         url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
         headers: {
@@ -13,17 +13,39 @@ function fetchData(repoOwner, repoName, cb) {
     
     request(options, function(err, res, body) {
         if (res.statusCode !== 200) throw new Error(`Request failed ${JSON.stringify(res, null, 2)}`);
-        cb(err, JSON.parse(body));
+        cb(err, JSON.parse(body), cb2);
+    });
+
+}
+
+function fetchStarredReposUrls(err, body, cb) {
+    let starredReposUrls = [];
+    body.forEach(user => {
+        starredReposUrls.push(user.starred_url);
+    });
+    cb(starredReposUrls);
+}
+
+function fetchStarredRepos(repoUrl) {
+    let repoDetails = {};
+    repoUrl.forEach((starredReposUrl) => {
+        request({
+            url: starredReposUrl.replace('{/owner}{/repo}', ''),
+            headers: {
+                'User-Agent': 'request', 
+                authorization: process.env.GITHUB_TOKEN
+            }
+        }, function(err, res, body) {
+            if (res.statusCode !== 200) throw new Error(`Request failed ${JSON.stringify(res, null, 2)}`);
+            else {
+                JSON.parse(body).forEach((repo) => {
+                    (repoDetails[repo.full_name] === undefined) ? repoDetails[repo.full_name] = 1 : repoDetails[repo.full_name] += 1;
+                })
+            }
+            console.log(repoDetails);
+        })
     });
 }
 
-fetchData(args[0], args[1], getStarredRepos);
 
-function getStarredRepos(err, body) {
-    let starredReposUrls = [];
-    body.forEach(user => {
-        starredReposUrls.push(user.starred_url)
-        console.log(starredReposUrls)
-    }) 
-}
-
+fetchUserData(args[0], args[1], fetchStarredReposUrls, fetchStarredRepos);
